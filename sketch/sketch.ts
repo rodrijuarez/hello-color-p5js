70f7d0f89d2276107d5fc999850fd6280af9022c
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-04-18 21:27:38 +0200

    Basic code

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -2,14 +2,23 @@ var sketch = (p: p5) => {
   p.preload = () => {};
 
   p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(720, 720);
+    p.noCursor();
   };
 
   p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    p.resizeCanvas(720, 720);
   };
 
-  p.draw = () => {};
+  p.draw = () => {
+    p.colorMode(p.HSB, 360, 100, 100);
+    p.rectMode(p.CENTER);
+    p.noStroke();
+    p.background(p.mouseY / 2, 100, 100);
+
+    p.fill(360 - p.mouseY / 2, 100, 100);
+    p.rect(360, 360, p.mouseX + 1, p.mouseX + 1);
+  };
 };
 
 var sketchP = new p5(sketch);
